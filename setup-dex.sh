1ae904cf8296e0228fff830ee8d240cab9ac4b3e
Andrei Epure <epure.andrei@gmail.com>
2017-02-01 21:59:46 +0000

    DEX to dexonline

--- setup-dex.sh
+++ setup-dex.sh
@@ -1,10 +1,9 @@
-mkdir /var/www/public
+sudo mkdir /var/www/public
 cd /var/www/public
-git clone https://github.com/dexonline/dexonline DEX
-cd DEX
+git clone https://github.com/dexonline/dexonline dexonline
+cd dexonline
 /bin/bash tools/setup.sh
-mysql -u root -p  -e "create database DEX character set utf8" scotchbox
+mysql -u root -p  -e "create database dexonline character set utf8" scotchbox
 wget -O /tmp/dex-database.sql.gz https://dexonline.ro/static/download/dex-database.sql.gz
-zcat /tmp/dex-database.sql.gz | mysql -u root -p DEX
+zcat /tmp/dex-database.sql.gz | mysql -u root -p dexonline
 php tools/migration.php
-
